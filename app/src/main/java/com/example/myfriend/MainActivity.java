21e307db0fd52328b91a7b44da783c455ad2b636
cabernal644 <cabernal644@hotmail.com>
2022-02-28 16:18:46 -0500

    se agrega

---
+++ app/src/main/java/com/example/myfriend/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myfriend;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.menuprincipal,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id =item.getItemId();
+        if(id == R.id.aprender){
+            Intent primeraPantalla = new Intent(this, MainActivity.class);
+            startActivity(primeraPantalla);
+        }
+        if(id == R.id.ingresar){
+            Intent segundaPantalla = new Intent(this, MainActivity3.class);
+            startActivity(segundaPantalla);
+        }
+        if(id == R.id.preguntar){
+            Intent terceraPantalla = new Intent(this, MainActivity2.class);
+            startActivity(terceraPantalla);
+        }
+        if(id == R.id.dialogo){
+            Intent cuartaPantalla = new Intent(this, MainActivity4.class);
+            startActivity(cuartaPantalla);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/myfriend/MainActivity3.java
@@ -0,0 +1,26 @@
+package com.example.myfriend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.Normalizer;
+import java.util.ArrayList;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
